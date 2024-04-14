9330d34837ccc72aefbf95f38fa97e3f2c3795cb
sisixili <113862606+sisixili@users.noreply.github.com>
2024-04-14 01:38:49 -0400

    Update database.js

--- database.js
+++ database.js
@@ -1,20 +1,16 @@
-// In package.json, type: module so can use ES modules (imports instead of common JS)
 import mysql from 'mysql2'
 
 import dotenv from 'dotenv'
 dotenv.config()
 
 // Environment variables on separate .env file (run npm i dotenv)
-export const pool = mysql.createPool({ // pool is collection of connections to db (pool of cxn can be reused)
-    host: process.env.MYSQL_HOST,  // 127.0.0.1 is same as localhost
+export const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST, 
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise() // To use async/await instead of callbacks
 
-// const result = await pool.query("SELECT * FROM notes") // result is an array of objects (first is db children, others are metadata)
-//const rows = result[0]
-// Put above actions in function for clarity
 export async function getNotes() {
     const [rows] = await pool.query("SELECT * FROM notes")
     return rows
@@ -25,9 +21,6 @@ export async function getNote(id) { // returns undefined if id not found
     WHERE id = ?
     `, [id])
     return rows[0]
-    // NOTE: WHERE id = ${id} is bad b/c the value is untrustworthy (prob come from http req from end user)
-    // Pass id as second param to avoid SQL injection attacks (user modify query to do anything)
-    // This is called a "prepared statement"
 }
 export async function createNote(title, content) {
     const [result] = await pool.query(`
